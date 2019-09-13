0c5929a9ccbb233eb4b465f21533c6307a887aad
FranLpz <franciscoalopezd@gmail.com>
2019-09-13 17:41:37 -0500

    Google Login

--- Login.js
+++ Login.js
@@ -1,6 +1,9 @@
 import React from 'react'
-import { StyleSheet, Text, TextInput, View, Button } from 'react-native'
+import { StyleSheet, Text, TextInput, View, Button, ImageBackground } from 'react-native'
 import firebase from 'react-native-firebase'
+import { GoogleSignin, GoogleSigninButton, statusCodes } from 'react-native-google-signin';
+
+GoogleSignin.configure();
 
 export default class Login extends React.Component {
   state = { email: '', password: '', errorMessage: null }
@@ -14,8 +17,35 @@ export default class Login extends React.Component {
       .catch(error => this.setState({ errorMessage: error.message }))
     console.log('handleLogin')
   }
+
+  onLoginOrRegister = async() => {
+    console.log('Yeiii!')
+    try {
+      await GoogleSignin.hasPlayServices();
+      const userInfo = await GoogleSignin.signIn();
+      this.setState({ userInfo });
+      console.log('1 ', userInfo)
+      this.props.navigation.navigate('Main');
+    } catch (error) {
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        console.log('error ', error.message)
+        // user cancelled the login flow
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        console.log('error ', error.message)
+        // operation (f.e. sign in) is in progress already
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        // play services not available or outdated
+        console.log('error ', error.message)
+      } else {
+        console.log('error ', error.message)
+        // some other error happened
+      }
+    }
+  }
+
   render() {
     return (
+      <ImageBackground source={require('./app/assets/photo_bkg.png')} style={{width: '100%', height: '100%'}}>
       <View style={styles.container}>
         <Text>Login</Text>
         {this.state.errorMessage &&
@@ -38,11 +68,19 @@ export default class Login extends React.Component {
           value={this.state.password}
         />
         <Button title="Login" onPress={this.handleLogin} />
-        <Button
-          title="Don't have an account? Sign Up"
-          onPress={() => this.props.navigation.navigate('SignUp')}
+        <Text style={{color: 'blue'}}
+          onPress={() => this.props.navigation.navigate('SignUp')}>
+          Don't have an account? Sign Up
+        </Text>
+        <GoogleSigninButton
+          style={{ width: 192, height: 48 }}
+          size={GoogleSigninButton.Size.Wide}
+          color={GoogleSigninButton.Color.Dark}
+          onPress={this.onLoginOrRegister}
+          disabled={this.state.isSigninInProgress} 
         />
       </View>
+      </ImageBackground>
     )
   }
 }
--- Main.js
+++ Main.js
@@ -1,23 +1,42 @@
 import React from 'react'
 import { StyleSheet, Platform, Image, Text, View, Button } from 'react-native'
+import { GoogleSignin } from 'react-native-google-signin';
 import firebase from 'react-native-firebase'
 
 export default class Main extends React.Component {
   state = { currentUser: null }
-  componentDidMount() {
-    const { currentUser } = firebase.auth()
+  async componentDidMount() {
+    const googleUser = await GoogleSignin.getCurrentUser();
+    const currentUser = googleUser.user || firebase.auth();
+    console.log('cu ',currentUser)
     this.setState({ currentUser })
   }
+  handleSignout = async() => {
+    try {
+      firebase.auth().signOut()
+      await GoogleSignin.revokeAccess();
+      await GoogleSignin.signOut();
+      this.setState({ user: null }); // Remember to remove the user from your app's state as well
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
 render() {
     const { currentUser } = this.state
+    const photoProfile = currentUser && currentUser.photo
 return (
       <View style={styles.container}>
+        <Image
+          style={{width: 80, height: 80}}
+          source={{uri:photoProfile}}
+        />
         <Text>
           Hi {currentUser && currentUser.email}!
         </Text>
         <Button
           title="Go Back"
-          onPress={() => firebase.auth().signOut()}
+          onPress={this.handleSignout }
         />
       </View>
     )
